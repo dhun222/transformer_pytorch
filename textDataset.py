c368d3e9808d45e624e1ac05780d598c5eb9d421
Hyun Donghoon <dhun0328@gmail.com>
2025-02-05 14:52:57 +0900

    training code added

--- textDataset.py
+++ textDataset.py
@@ -28,25 +28,29 @@ class TranslationDataset(Dataset):
         return dept, dest
 
 class IdxSampler(Sampler):
-    def __init__(self, idx, start=0):
-        self.idx = idx[start:]
-        self.idx.extend(idx[:start])
+    def __init__(self, idx):
+        self.idx = idx
 
     def __iter__(self):
         return iter(self.idx)
     
     def __len__(self):
         return len(self.idx)
+    
+    def set_start(self, start):
+        temp = self.idx
+        self.idx = self.idx[start:]
+        self.idx.extend(temp[:start])
 
-def get_and_split_dataset(table, tokenizer, max_len, valid_ratio, start):
+def get_and_split_dataset(table, tokenizer, max_len, valid_ratio):
 
     dataset = TranslationDataset(table, tokenizer, max_len)
 
     n = len(dataset)
-    n_train = int(n * valid_ratio)
+    n_train = n - int(n * valid_ratio)
     train_idx = list(range(n_train))
     valid_idx = list(range(n_train, n))
-    train_sampler = IdxSampler(train_idx, start)
+    train_sampler = IdxSampler(train_idx)
     valid_sampler = IdxSampler(valid_idx)
 
     return dataset, train_sampler, valid_sampler
--- train.py
+++ train.py
@@ -18,16 +18,24 @@ class Scheduler(torch.optim.lr_scheduler.LRScheduler):
         return [self._d_model * min(pow(self._step_count, -0.5), self._step_count * pow(self.warmup_steps, -1.5))]
 
 def train(h, data_path, ckpt_path):
+    print("Model Configuration:")
+    print(f"\tNumber of layers: {h.N}")
+    print(f"\td_model: {h.d_model}")
+    print(f"\tnum_heads: {h.num_heads}")
+    print(f"\td_ff: {h.d_ff}")
+    print(f"\tDropout: {h.dropout}")
+
+    print("\nTraining configuration: ")
+    print(f"\tEpoch: {h.epoch}")
+    print(f"\tvalid_ratio: {h.valid_ratio}")
+    print(f"\tWarmup steps: {h.warmup_steps}")
+    print(f"\tBatch size: {h.batch_size}")
+    print(f"\tnum_workers: {h.num_workers}")
+    print(f"\tbeta1: {h.beta1}")
+    print(f"\tbeta2: {h.beta2}")
+    print(f"\teps: {h.eps}\n\n")
+    
     device = h.device
-    print('loading data...')
-    table = load_data(data_path)
-    print('done')
-
-    if os.path.exists(h.tokenizer_path):
-        tokenizer = load_tokenizer(h.tokenizer_path)
-    else:
-        tokenizer = build_tokenizer(h.tokenizer_path, table, max_len=h.max_len, vocab_size=h.vocab_size)
-
     transformer = Transformer(
         N=h.N, 
         d_model=h.d_model, 
@@ -39,25 +47,40 @@ def train(h, data_path, ckpt_path):
     ).to(device)
     print('model built')
 
+    if os.path.exists(h.tokenizer_path):
+        tokenizer = load_tokenizer(h.tokenizer_path)
+    else:
+        tokenizer = build_tokenizer(h.tokenizer_path, table, max_len=h.max_len, vocab_size=h.vocab_size)
+
+    print('loading data...')
+    table = load_data(data_path)
+    print('done')
+
     optimizer = torch.optim.Adam(transformer.parameters(), betas=(h.beta1, h.beta2), eps=h.eps)
     scheduler = Scheduler(optimizer, h.d_model, h.warmup_steps)
+
+    dataset, train_sampler, valid_sampler = get_and_split_dataset(table, tokenizer, h.max_len, h.valid_ratio)
+    
+    train_loader = DataLoader(dataset, batch_size=h.batch_size, num_workers=h.num_workers, sampler=train_sampler)
+    valid_loader = DataLoader(dataset, batch_size=h.batch_size, num_workers=h.num_workers, sampler=valid_sampler) 
+    print('data split done')
 
     epoch = 0
     batch = 0
     if os.path.exists(ckpt_path):
         ckpt = torch.load(ckpt_path)
         print('loading ckpt...')
-        epoch = ckpt['e']
+        epoch = ckpt['epoch']
         batch = ckpt['batch']
         transformer.load_state_dict(ckpt['model_state_dict'])
         optimizer.load_state_dict(ckpt['optimizer_state_dict'])
         scheduler.load_state_dict(ckpt['scheduler_state_dict'])
-
-    dataset, train_sampler, valid_sampler = get_and_split_dataset(table, tokenizer, h.max_len, h.valid_ratio, batch)
+        print(f'epoch: {epoch}, batch: {batch} / {len(train_loader)}')
     
-    train_loader = DataLoader(dataset, batch_size=h.batch_size, num_workers=h.num_workers, sampler=train_sampler)
-    valid_loader = DataLoader(dataset, batch_size=h.batch_size, num_workers=h.num_workers, sampler=valid_sampler) 
-    print('data split done')
+    # to start training from in the middle of dataset. There might be better way...
+    train_loader_iter = train_loader.__iter__()
+    for i in tqdm(range(batch)):
+        train_loader_iter.__next__()
 
     loss_function = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
     
@@ -67,38 +90,60 @@ def train(h, data_path, ckpt_path):
         print(f'epoch: {e + 1}')
 
         transformer.train()
-        for i, (dept, dest) in enumerate(tqdm(train_loader)):
+        train_len = len(train_loader)
+        b_time = 0
+        while True:
+            try:
+                dept, dest = train_loader_iter.__next__()
+                b_start = time()
+                print(f"\r", end="")
+                progress = int((batch / train_len) * 50)
+                for i in range(50):
+                    if i < progress:
+                        print("|", end="")
+                    else:
+                        print(" ", end="")
+                print(f": {batch} / {train_len}. {b_time}/batch. {(train_len - batch) * b_time}", end="")
 
-            optimizer.zero_grad()
-            dept = dept.to(device)
-            dest = dest.to(device)
+                optimizer.zero_grad()
+                dept = dept.to(device)
+                dest = dest.to(device)
 
-            pred = transformer(dept, dest)
-            loss = loss_function(pred.reshape(-1, h.vocab_size), dest.reshape(-1, 1).squeeze(1))
-            loss_val = loss.item()
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
+                pred = transformer(dept, dest)
+                loss = loss_function(pred.reshape(-1, h.vocab_size), dest.reshape(-1, 1).squeeze(1))
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
 
-            step += 1
-            if step % 10000 == 0:
-                torch.save(
-                    {
-                        'epoch': e, 
-                        'batch': i, 
-                        'optimizer_state_dict': optimizer.state_dict(), 
-                        'scheduler_state_dict': scheduler.state_dict(), 
-                        'model_state_dict': transformer.state_dict(), 
-                    }, 
-                    ckpt_path
-                )
+                batch += 1
+                step += 1
+                if step % 10000 == 0:
+                    torch.save(
+                        {
+                            'epoch': e, 
+                            'batch': batch, 
+                            'optimizer_state_dict': optimizer.state_dict(), 
+                            'scheduler_state_dict': scheduler.state_dict(), 
+                            'model_state_dict': transformer.state_dict(), 
+                        }, 
+                        ckpt_path
+                    )
+                
+                b_end = time()
+                b_time = b_end - b_start
+            except StopIteration:
+                print("break")
+                break
 
         transformer.eval()
         valid_error = 0
-        for (dept, dest) in valid_loader:
+        print("validation")
+        for (dept, dest) in tqdm(valid_loader):
+            dept = dept.to(device)
+            dest = dest.to(device)
             with torch.no_grad():
                 pred =transformer(dept, dest)
-                valid_error += loss_function(pred, dept).item()
+                valid_error = loss_function(pred.reshape(-1, h.vocab_size), dest.reshape(-1, 1).squeeze(1)).item()
 
         end_time = time()
         print(f'time: {end_time - start_time}, valid error: {valid_error / len(valid_sampler)}')
